e41cc57b244a6f85773c44184da13c5d52b75dd3
nehemiagueldi <nehemia.gueldi@student.umn.ac.id>
2024-10-17 15:20:46 +0700

    Create About Section
    - Make Bento Grid
    - Add animation and make responsive grid
    - Create email can copied
    - Make short of about me

--- app/page.tsx
+++ app/page.tsx
@@ -3,30 +3,16 @@ import Hero from "./../components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
 import { FaInfo, FaPhone } from "react-icons/fa6";
+import Grid from "@/components/Grid";
+import { navItems } from "@/data";
 
 export default function Home() {
-  const navItems = [
-    {
-      name: "Home",
-      link: "/",
-      icon: <FaHome />,
-    },
-    {
-      name: "About",
-      link: "/about",
-      icon: <FaInfo />,
-    },
-    {
-      name: "Contact",
-      link: "/contact",
-      icon: <FaPhone />,
-    },
-  ];
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems}/>
         <Hero />
+        <Grid />
       </div>
     </main>
   );
